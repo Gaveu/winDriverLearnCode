814b75a9bedb3d270025717a1405a09ef318645d
-Gaveu <gaveu086@163.com>
2020-02-24 22:48:20 +0800

    更新关于查询注册表键、枚举注册表子键和键值的实验代码与思维导图

--- 05/Registry/driver.c
+++ 05/Registry/driver.c
@@ -157,13 +157,132 @@ VOID KeyOpenTest()
 
 }
 
+VOID KeyEnum()
+{
+	ULONG i;
+	HANDLE hKey;
+	NTSTATUS status;
+	OBJECT_ATTRIBUTES oa;
+	UNICODE_STRING KeyPath = RTL_CONSTANT_STRING(L"\\Registry\\Machine\\SYSTEM\\Setup");
+
+	InitializeObjectAttributes(
+		&oa,						//返回的属性对象
+		&KeyPath,					//键的路径
+		OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE,	//区分大小写、内核对象
+		NULL,						//根路径
+		NULL						//安全描述符
+		);
+	status = ZwOpenKey(&hKey, KEY_ALL_ACCESS, &oa);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("Key Open Failed!%x\n", status));
+		return;
+	}
+
+	//获取hKey所对应的键的信息
+	ULONG ulRet;
+	PKEY_FULL_INFORMATION pkfi = (PKEY_FULL_INFORMATION)ExAllocatePool(PagedPool, 1024);
+	if (!pkfi)
+	{
+		KdPrint(("PagedPool Allocate Failed!%x\n", STATUS_INSUFFICIENT_RESOURCES));
+		ZwClose(hKey);
+		return;
+	}
+	RtlZeroMemory(pkfi, 1024);
+	status = ZwQueryKey(
+		hKey,				//待查询键的句柄
+		KeyFullInformation,	//返回的键信息类型
+		pkfi,				//返回输出的缓冲区
+		1024,				//输出的缓冲区长度
+		&ulRet				//返回在输出缓冲区写入数据的长度
+		);
+	if (!NT_SUCCESS(status))
+	{
+		KdPrint(("Key Query Failed!%x\n", status));
+		ZwClose(hKey);
+		return;
+	}
+	PKEY_BASIC_INFORMATION pbi = (PKEY_BASIC_INFORMATION)ExAllocatePool(PagedPool, 1024);
+	if (!pbi)
+	{
+		KdPrint(("PagedPool Allocate Failed!%x\n", STATUS_INSUFFICIENT_RESOURCES));
+		ZwClose(hKey);
+		ExFreePool(pkfi);
+		return;
+	}
+	RtlZeroMemory(pbi, 1024);
+	PKEY_VALUE_FULL_INFORMATION pkvfi = (PKEY_VALUE_FULL_INFORMATION)ExAllocatePool(PagedPool, 1024);
+	if (!pkvfi)
+	{
+		KdPrint(("PagedPool Allocate Failed!%x\n", STATUS_INSUFFICIENT_RESOURCES));
+		ZwClose(hKey);
+		ExFreePool(pkfi);
+		ExFreePool(pbi);
+		return;
+	}
+	RtlZeroMemory(pkvfi, 1024);
+
+	for (i = 0; i < pkfi->SubKeys; ++i)
+	{	//枚举hKey对应键下的所有子键
+		RtlZeroMemory(pbi, 1024);
+		status = ZwEnumerateKey(
+			hKey,	//待枚举子键的键句柄
+			i,		//表示被枚举的子键序号
+			KeyBasicInformation,//枚举子键返回的信息类型
+			pbi,	//返回信息的缓冲区
+			1024,	//缓冲区长度
+			&ulRet	//返回写入数据的长度
+			);
+		if (!NT_SUCCESS(status))
+		{
+			KdPrint(("SubKey Enumerate Failed!%x\n", status));
+			ZwClose(hKey);
+			ExFreePool(pkfi);
+			ExFreePool(pbi);
+			ExFreePool(pkvfi);
+			return;
+		}
+		KdPrint(("SubKey:%ls\n", pbi->Name));	//输出子键名
+	}
+
+	for (i = 0; i < pkfi->Values; ++i)
+	{	//枚举hKey对应键下的所有键值
+		RtlZeroMemory(pkvfi, 1024);
+		status = ZwEnumerateValueKey(
+			hKey,	//待枚举子键的键句柄
+			i,		//表示被枚举的子键序号
+			KeyValueFullInformation,//枚举键值返回的信息类型
+			pkvfi,	//返回信息的缓冲区
+			1024,	//缓冲区长度
+			&ulRet	//返回写入数据的长度
+			);
+		if (!NT_SUCCESS(status))
+		{
+			KdPrint(("Value Enumerate Failed!%x\n", status));
+			ZwClose(hKey);
+			ExFreePool(pkfi);
+			ExFreePool(pbi);
+			ExFreePool(pkvfi);
+			return;
+		}
+		KdPrint(("Value:%ls\n", pkvfi->Name));	//输出键值名
+	}
+
+	KdPrint(("Key Enumerate Successed!\n"));
+
+	ZwClose(hKey);
+	ExFreePool(pkfi);
+	ExFreePool(pbi);
+	ExFreePool(pkvfi);
+}
+
 NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
 {
 
 	KdPrint(("Hello Driver!\n"));
 	DriverObject->DriverUnload = Unload;
 
-	KeyOpenTest();
+	KeyEnum();
 
 	return STATUS_SUCCESS;
 }
