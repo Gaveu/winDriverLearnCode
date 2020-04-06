b8ce6b1fc78af0bdefb8aa62a512aaaeeda54e99
-Gaveu <gaveu086@163.com>
2020-04-06 21:31:56 +0800

    添加关于线程枚举的功能代码

---
+++ ThreadOpeartions/test.c
@@ -0,0 +1,23 @@
+#include"drvHead.h"
+VOID Unload(IN PDRIVER_OBJECT DriverObject)
+{
+	//PsResume((HANDLE)2424);	//虚拟机内运行calc.exe进程对应pid为2424，执行成功时该进程继续执行
+
+	KdPrint(("GoodBye Driver!\n"));
+}
+
+NTSTATUS DriverEntry(IN PDRIVER_OBJECT DriverObject, IN PUNICODE_STRING RegistryPath)
+{
+	KdPrint(("Hello Driver!\n"));
+	DriverObject->DriverUnload = Unload;
+
+	//PsEnum1();
+	//PsEnum2();
+	//PsSuspend((HANDLE)2424);	//虚拟机内运行calc.exe进程对应pid为2424，执行成功时该进程暂停执行
+	//PsTerminate((HANDLE)2548);	//虚拟机内运行calc.exe进程对应pid为2548，执行成功时该进程结束执行
+
+	PsEnum2();
+
+	return STATUS_SUCCESS;
+}
+
---
+++ ThreadOpeartions/thFunc.c
@@ -0,0 +1,72 @@
+#include"drvHead.h"
+
+//Ïß³ÌÃ¶¾Ù
+VOID ThEnum1()
+{
+	PETHREAD pEth = NULL;
+	PEPROCESS pEproc = NULL;
+	NTSTATUS status;
+	ULONG ulThreadId = 0;
+	ULONG ulPid = 0;
+	PCHAR pProcName = NULL;
+
+	KdPrint(("ThEnum1\n"));
+	for (ulThreadId = 0; ulThreadId < 80000; ulThreadId += 4)
+	{
+		status = PsLookupThreadByThreadId((HANDLE)ulThreadId, &pEth);
+		if (NT_SUCCESS(status))
+		{
+			pEproc = (PEPROCESS)(*(PEPROCESS *)((PCHAR)pEth + 0x150));
+			ulPid = (ULONG)PsGetProcessId(pEproc);
+			pProcName = PsGetProcessImageFileName(pEproc);
+			KdPrint(("Tid:%d\tPid:%d\t%s\n", ulThreadId, ulPid, pProcName));
+		}
+	}
+}
+
+VOID ThEnum2(PEPROCESS process)
+{
+	PETHREAD pEth;
+	PETHREAD pEthFirst;
+	PEPROCESS pEproc = process;
+	PLIST_ENTRY pLe = NULL;
+	NTSTATUS status;
+	ULONG ulThreadId;
+
+	KdPrint(("ThEnum2\n"));
+	//KdBreakPoint();
+	pLe = (PLIST_ENTRY)((PCHAR)pEproc + 0x2c);
+	pLe = pLe->Flink;
+	pEth = (PETHREAD)((PCHAR)pLe - 0x1e0);
+	pEthFirst = pEth;
+	do
+	{
+		ulThreadId = *(PULONG)((PCHAR)pEth + 0x230);
+		if (ulThreadId)
+		{
+			KdPrint(("\tTid:%d\n", ulThreadId));
+		}
+		pLe = (PLIST_ENTRY)((PCHAR)pEth + 0x1e0);
+		pLe = pLe->Flink;
+		pEth = (PETHREAD)((PCHAR)pLe - 0x1e0);
+	} while (pEth != pEthFirst);
+}
+
+VOID ThEnum3(PEPROCESS process)
+{
+	PETHREAD pThd = NULL;
+	NTSTATUS status;
+	ULONG tid;
+	KdPrint(("ThEnum2\n"));
+	for (tid = 0; tid < 20000; tid += 4)
+	{
+		status = PsLookupThreadByThreadId((HANDLE)tid, &pThd);
+		if (NT_SUCCESS(status))
+		{
+			if (IoThreadToProcess(pThd) == process)
+			{
+				KdPrint(("\tTid:%d\n", tid));
+			}
+		}
+	}
+}
